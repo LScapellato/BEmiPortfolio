cc4d9ebca3cbdda2982bcf94e5c616f3ac0afea0
LScapellato <94746621+LScapellato@users.noreply.github.com>
2022-05-17 12:12:43 -0300

    Implementando
    Test implementacion

--- src/main/java/com/abmlspring/practica/PortfolioApplication.java
+++ src/main/java/com/abmlspring/practica/PortfolioApplication.java
@@ -19,7 +19,7 @@ public class PortfolioApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+				registry.addMapping("/**").allowedOrigins("https://miportfolioimg.web.app").allowedMethods("*").allowedHeaders("*");
 			}
 		};
 	}
